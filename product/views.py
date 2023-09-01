9cbfe831860c1f7a87a63d93b2b2c687e45a3cd0
muhmedwagdy <mohamed.wagdy2016@gmail.com>
2023-09-01 17:58:48 +0300

    add query set api test from data in front

--- product/views.py
+++ product/views.py
@@ -1,14 +1,49 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.views.generic import ListView,DetailView
 from .models import Product,Brand,ProductImages,Review
+from django.db.models import Q , F
+from django.db.models.aggregates import Min,Max,Sum,Count,Avg
+
 
 
 
 def queryset_debug(request):
 
-    data = Product.objects.all()
+    # data = Product.objects.select_related('brand').all()
+    # data =  Product.objects.filter(price__gt= 20)
+    # data =  Product.objects.filter(price__lte= 40)
+    # data =  Product.objects.filter(price__range=(70,90))
+    # data =  Product.objects.filter(brand___name='apple')
+    # data =  Product.objects.filter(brand__price__gt=30)
+    # data = Product.objects.filter(name__startswith='Kathy')
+    # data = Product.objects.filter(name__endswith='Kathy')
+    # data = Product.objects.filter(name__endswith='Mack')
+    # data = Product.objects.filter(tags__isnull=True)
+    # data = Review.objects.filter(created_at__year = 2023)
+    # data = Product.objects.filter(price__gt=80 , quantity__lt=20)
+    # data = Product.objects.filter(Q(price__gt=80) | Q(quantity__lt=20))
+    # data = Product.objects.filter(~Q(price__gt=80) |Q(quantity__lt=20))
+    # data = Product.objects.filter(price = F('quantity'))
+    # data = Product.objects.all().order_by('name')
+    # data = Product.objects.all().order_by('-name')
+    # data = Product.objects.all()[10:40]
+    # data = Product.objects.values('name','price','brand__name')
+    # data = Product.objects.values('name','price')
+   
+    # data = Product.objects.defer('slug','discription')
+
+    data = Product.objects.values_list('name','price')
+
+    # data = Product.objects.aaggregate(Sum='quantaity')
+    # data = Product.objects.aaggregate(Avg=Price)
+
+
+
+
+
+
+
+
     return render(request,'product/debug.html',{'data':data})
 
 
