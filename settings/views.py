95a2388c8b1ca05208e4d9f5f62ce938a06b72a9
muhmedwagdy <mohamed.wagdy2016@gmail.com>
2023-09-09 17:06:49 +0300

    add new rate and feedback items

--- settings/views.py
+++ settings/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.db.models import Count
+
+from product.models import Product,Brand,Review
+
+
+
+def home(request):
+
+    brands = Brand.objects.all().annotate(product_count=Count('product_brand'))
+    sale_products = Product.objects.filter(flag='Sale')[:10]
+    featured_products = Product.objects.filter(flag='Feature')[:6]
+    new_products =  Product.objects.filter(flag='New')[:10]
+
+    reviews = Review.objects.all()[:5]
+
+
+    return render(request,'settings/home.html',{
+        'brands':brands,
+        'sale_products':sale_products,
+        'featured_products':featured_products,
+        'new_products':new_products,
+        'reviews':reviews
+    })
