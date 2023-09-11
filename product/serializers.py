ca599a1165a46577b31a65c778d75461801b7c59
muhmedwagdy <mohamed.wagdy2016@gmail.com>
2023-09-11 14:04:06 +0300

    add list detail in  products and display name

--- product/serializers.py
+++ product/serializers.py
@@ -1,10 +1,22 @@
 from rest_framework import serializers
 
-from .models import Product,Brand
+from .models import Product,Brand,Review
 
 from django.db.models.aggregates import Avg
 
+class BrandListSerilizer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Brand
+        fields = '__all__'
+
+
+
+
+
 class ProductListSerilizer(serializers.ModelSerializer):
+    brand = serializers.StringRelatedField()
+    # brand = BrandListSerilizer()
     avg_rate = serializers.SerializerMethodField()
     reviews_count = serializers.SerializerMethodField()
     price_with_tax = serializers.SerializerMethodField()
@@ -29,20 +41,38 @@ class ProductListSerilizer(serializers.ModelSerializer):
         reviews = product.review_product.all().count()
         return reviews
 
+class ReviewsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Review
+        fields = '__all__'
 
 
 
 
 class ProductDetailSerilizer(serializers.ModelSerializer):
+    brand = serializers.StringRelatedField()
+   
+    avg_rate = serializers.SerializerMethodField()
+    reviews_count = serializers.SerializerMethodField()
+    reviews = ReviewsSerializer(source='review_product',many=True)
+
     class Meta:
         model = Product
         fields = '__all__'
 
-class BrandListSerilizer(serializers.ModelSerializer):
+    def get_avg_rate(self,product):
+        avg = product.review_product.aggregate(rate_avg=Avg('rate'))
+        if not avg['rate_avg']:
+                result = 0
+                return result
+        return avg['rate_avg']
     
-    class Meta:
-        model = Brand
-        fields = '__all__'
+
+    def get_reviews_count(self,product:Product):
+        reviews = product.review_product.all().count()
+        return reviews
+
+
 
 
 
